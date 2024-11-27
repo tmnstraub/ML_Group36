51fdac88c7f35a40d61459d8b0296dbddbc7e8f7
Tim Straub <tmnstraub@gmail.com>
2024-11-27 10:06:55 +0000

    new features functions

--- src/Preprocessing_functions.py
+++ src/Preprocessing_functions.py
@@ -13,6 +13,72 @@ from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 
+# Create new features based on the binned groups of the original features
+def newFeature_binnedGroups(X_train, X_val, X_test, columns, bins=4):
+    '''
+    Create new features based on the binned groups of the original features
+
+    Parameters:
+    X_train: DataFrame
+    X_val: DataFrame
+    X_test: DataFrame
+    columns: list
+    bins: int
+    '''
+
+    for col in columns:
+        # Define bins based on training data
+        train_bins = pd.qcut(X_train[col], q=bins, retbins=True)[1]  # Get bin edges
+
+        # Apply the bins to all datasets
+        X_train[f'{col} Group'] = pd.cut(X_train[col], bins=train_bins, labels=False, include_lowest=True)
+        X_val[f'{col} Group'] = pd.cut(X_val[col], bins=train_bins, labels=False, include_lowest=True)
+        X_test[f'{col} Group'] = pd.cut(X_test[col], bins=train_bins, labels=False, include_lowest=True)
+
+        X_train[f'{col} Group'] = X_train[f'{col} Group'].astype(str)
+        X_val[f'{col} Group'] = X_val[f'{col} Group'].astype(str)
+        X_test[f'{col} Group'] = X_test[f'{col} Group'].astype(str)
+
+    return X_train, X_val, X_test
+
+# Create new feature month based on the date feature
+def newFeature_month(X_train, X_val, X_test, columns):
+    '''
+    Create new feature month based on the date feature. 
+    Need to be applied to columns with datetime format.
+
+    Parameters:
+    X_train: DataFrame
+    X_val: DataFrame
+    X_test: DataFrame
+    date_col: str
+    '''
+
+    for col in columns:
+        X_train[f'{col} Month'] = X_train[col].dt.month
+        X_val[f'{col} Month'] = X_val[col].dt.month
+        X_test[f'{col} Month'] = X_test[col].dt.month
+
+    return X_train, X_val
+
+# Create new feature days since the last event
+def newFeature_daysBetween(X_train, X_val, X_test, firstDate, secondDate):
+    '''
+    Create new feature days since the last event. 
+    Need to be applied to columns with datetime format.
+
+    Parameters:
+    X_train: DataFrame
+    X_val: DataFrame
+    X_test: DataFrame
+    date_col: str
+    '''
+
+    X_train[f'Days Between {firstDate} and {secondDate}'] = (X_train[secondDate].max() - X_train[firstDate]).dt.days
+    X_val[f'Days Between {firstDate} and {secondDate}'] = (X_val[secondDate].max() - X_val[firstDate]).dt.days
+    X_test[f'Days Between {firstDate} and {secondDate}'] = (X_test[secondDate].max() - X_test[firstDate]).dt.days
+
+    return X_train, X_val, X_test
 
 
 # Push outliers to upper and lower bounds
@@ -81,6 +147,10 @@ def encoding_onehot(X_train, X_val, columns):
     '''
     OneHot Encoder for categorical variables with low cardinality
     '''
+    # Changing datatype to string for encoding
+    X_train[columns] = X_train[columns].astype(str)
+    X_val[columns] = X_val[columns].astype(str)
+
     ohe = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
     X_train_encoded = pd.DataFrame(ohe.fit_transform(X_train[columns]))
     X_val_encoded = pd.DataFrame(ohe.transform(X_val[columns]))
@@ -92,6 +162,10 @@ def encoding_frequency1(X_train, X_val, columns):
     Frequency Encoding for categorical variables with high cardinality -> not filling unseen values
     '''
     for col in columns:
+        # Changing datatype to string for encoding
+        X_train[col] = X_train[col].astype(str)
+        X_val[col] = X_val[col].astype(str)
+
         fe = X_train.groupby(col).size() / len(X_train)
         X_train[col] = X_train[col].apply(lambda x : fe[x])
         X_val[col] = X_val[col].apply(lambda x : fe[x])
@@ -102,7 +176,12 @@ def encoding_frequency2(X_train, X_val, columns):
     '''
     Frequency Encoding for categorical variables with high cardinality -> filling unseen values
     '''
+    
     for col in columns:
+        # Changing datatype to string for encoding
+        X_train[col] = X_train[col].astype(str)
+        X_val[col] = X_val[col].astype(str)
+
         frequency_map = X_train[col].value_counts(normalize=True)
         X_train[col] = X_train[col].map(frequency_map)
         X_val[col] = X_val[col].map(frequency_map).fillna(0)
